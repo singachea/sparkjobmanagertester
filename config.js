d2f3a0c2e4fff54c7b82d6f3c6abb6d113d9fa3a
Ream Lim <rlim@humanlongevity.com>
2016-05-30 14:48:23 +0800

    update job config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 let _ = require('lodash');
+let uid = require('node-uuid');
 let host = process.env.SJMTEST_HOST || 'http://localhost:8090'
 
 module.exports = {
@@ -34,11 +35,27 @@ module.exports = {
         }
       },
       cohortJob: {
-        className: "sjm.jobs.SqlJob",
+        className: "hli.jobs.CohortQueryJob",
         body: () => {
+          var uuid = uid.v1();
+          let result_location = `hdfs://10.2.33.211:50030/test/data-ungoverned/query-service-test/cq03${uuid}.parquet`;
+
           return {
-            sql : "SELECT DISTINCT p.chrom, p.pos, p.ref, p.alt, p.highest_clinical_significance, p.variant_disease_name, p.rs_id, p.hli_allele_frequency, p.sample_count, p.gene_name, p.annotation, p.hgvs_c, p.hgvs_p, p.feature_type, p.feature_id, p.cadd_score, p.high_conf_region, CASE WHEN length(p.ref) = 1 and length(p.alt) = 1 THEN 'SNV' WHEN length(p.ref) > 1 and length(p.alt) = 1 THEN 'DEL' WHEN length(p.ref) = 1 and length(p.alt) > 1 THEN 'INS' ELSE 'COMPLEX' END AS variant_type FROM app_qs.annotation_kb_prejoint AS p WHERE (p.rs_id = \"rs1799759\") OR (p.chrom = \"chrX\" AND p.pos = \"74529484\" AND p.ref = \"T\" AND p.alt = \"G\") OR (p.gene_name = \"FAAH2\" AND p.hgvs_c = \"c.1404T>C\") OR (p.gene_name = \"RPGR\" AND p.hgvs_p = \"p.Ala388Ala\") ",
-            cacheDf: false
+            query: {
+              result_location: result_location,
+              sql: ["SELECT DISTINCT p.chrom, p.pos, p.ref, p.alt, p.highest_clinical_significance, p.variant_disease_name, p.rs_id, p.hli_allele_frequency, p.sample_count, p.gene_name, p.annotation, p.hgvs_c, p.hgvs_p, p.feature_type, p.feature_id, p.cadd_score, p.high_conf_region, CASE WHEN length(p.ref) = 1 and length(p.alt) = 1 THEN 'SNV' WHEN length(p.ref) > 1 and length(p.alt) = 1 THEN 'DEL' WHEN length(p.ref) = 1 and length(p.alt) > 1 THEN 'INS' ELSE 'COMPLEX' END AS variant_type FROM app_qs.annotation_kb_prejoint AS p WHERE (p.rs_id = \"rs1799759\") OR (p.chrom = \"chrX\" AND p.pos = \"74529484\" AND p.ref = \"T\" AND p.alt = \"G\") OR (p.gene_name = \"FAAH2\" AND p.hgvs_c = \"c.1404T>C\") OR (p.gene_name = \"RPGR\" AND p.hgvs_p = \"p.Ala388Ala\") "]
+            },
+            cache: {
+              result_location: result_location,
+              redis_connection: "v2-ccm-dev2-001.owbwwo.0001.usw2.cache.amazonaws.com",
+              sections: [
+                {
+                  cache_key: "cq-0003-0#{uuid}",
+                  offset: 0,
+                  limit: 1000
+                }
+              ]
+            }
           }
         }
       },
